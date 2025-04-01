488b6fa1d746bd1323a7a176b69860d695d510bd
alekma12 <alekmcv@gmail.com>
2025-04-01 17:12:50 -0400

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoryCardComponent } from './components/category-card/category-card.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoryCardComponent,
+    ProductCardComponent,
+    ShoppingCartComponent,
+    
   ],
   imports: [
     BrowserModule,
